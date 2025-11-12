e2ccf56654f6d844c8c2a32421a4195842bedf3e
argaDkze <muhammadargasetiawan1607@gmail.com>
2025-11-12 12:04:27 +0700

    testingcommitpart1.cpp

---
+++ partmenuwenno.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+	void warteg (){
+		int prices[7] = {23,23,12,34,12,34,12};
+		vector <string> pesanan;
+		int hargatotal=0;
+		string makanan[7] = {"mie ayam boyolali", "bakso fuad", "sate ambabull", "nasi padang", "nasi goreng mas narji", "es teh simanis", "kopi hitam jawa"};
+		
